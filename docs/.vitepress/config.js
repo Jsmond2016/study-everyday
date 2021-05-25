c0fdefaec40f2263bfe48ad3f2538107f9f7727e
Jsmond2016 <Jsmond2016@gmail.com>
2021-05-25 23:28:31 +0800

    update notes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -39,6 +39,8 @@ function getGuideSidebar() {
         {
           text: '5月',
           children: [
+            { text: '0525', link: '/record/05/0525' },
+            { text: '0524', link: '/record/05/0524' },
             { text: '0521', link: '/record/05/0521' },
             { text: '0520', link: '/record/05/0520' },
             { text: '0519', link: '/record/05/0519' },
