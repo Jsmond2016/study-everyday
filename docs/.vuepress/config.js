65cab2bd7bd0080dabde63597e6fd05e32a3b63a
Jsmond2016 <Jsmond2016@gmail.com>
2022-01-03 15:20:11 +0800

    变更为 vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,57 @@
+// const taskLists = require('markdown-it-task-lists');
+const archive2021Sidebar = require('./sidebar.archive-2021');
+
+module.exports = {
+  lang: 'zh-CN',
+  title: '每日学习',
+  description: '前端进阶每日学习',
+  base: "/study-everyday/",
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    repo: 'Jsmond2016/study-everyday',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '编辑本页',
+    lastUpdated: '最后更新时间',
+    nav: [],
+    sidebar: getGuideSidebar(),
+  },
+  // markdown: {
+  //   extendsMarkdown: (md) => {
+  //     console.log('md: ', md);
+  //     // use more markdown-it plugins!
+  //     md.use(taskLists)
+  //   }
+  // }
+}
+
+function getGuideSidebar() {
+  return [
+    {
+      text: '每日学习',
+      children: [
+        { text: '2022年度目标', link: '/record/goal-2022' },
+        {
+          text: '随笔文章',
+          children: [
+            { text: '如何高效学习', link: '/record/read-notes/book-1' },
+            { text: '如何做前端技术设计', link: '/record/read-notes/book-2' },
+            { text: 'Docker教程', link: '/record/read-notes/docker-note' },
+            { text: 'leetcode难度分类', link: '/record/read-notes/leetcode-retag' },
+            { text: 'leetcode目录', link: '/record/read-notes/leetcode-category' },
+          ]
+        },
+        { text: '模板', link: '/record/template' },
+        { text: 'TODOS', link: '/record/todos' },
+        { text: '面试冲刺', link: '/record/fe-interview' },
+        { text: '手写js', link: '/record/hand-js' },
+      ]
+    },
+    {
+      text: 'archives',
+      collapsible: true,
+      children: archive2021Sidebar.map(item => ({ ...item, collapsible: true }))
+    }
+
+  ]
+}
---
+++ docs/.vuepress/sidebar.archive-2021.js
@@ -0,0 +1,795 @@
+module.exports = [
+  { text: '2021年度目标', link: '/record/goal-2021' },
+  { text: '10月目标', link: '/record/target-month-10' },
+  { text: '8月目标', link: '/record/target-month-8' },
+  { text: '7月目标', link: '/record/target-month-7' },
+  { text: '6月目标', link: '/record/target-month-6' },
+  {
+    "text": "12月",
+    "children": [
+      {
+        "text": "1231",
+        "link": "/record/12/1231"
+      },
+      {
+        "text": "1230",
+        "link": "/record/12/1230"
+      },
+      {
+        "text": "1229",
+        "link": "/record/12/1229"
+      },
+      {
+        "text": "1228",
+        "link": "/record/12/1228"
+      },
+      {
+        "text": "1227",
+        "link": "/record/12/1227"
+      },
+      {
+        "text": "1226",
+        "link": "/record/12/1226"
+      },
+      {
+        "text": "1224",
+        "link": "/record/12/1224"
+      },
+      {
+        "text": "1223",
+        "link": "/record/12/1223"
+      },
+      {
+        "text": "1222",
+        "link": "/record/12/1222"
+      },
+      {
+        "text": "1221",
+        "link": "/record/12/1221"
+      },
+      {
+        "text": "1220",
+        "link": "/record/12/1220"
+      },
+      {
+        "text": "1219",
+        "link": "/record/12/1219"
+      },
+      {
+        "text": "1218",
+        "link": "/record/12/1218"
+      },
+      {
+        "text": "1217",
+        "link": "/record/12/1217"
+      },
+      {
+        "text": "1216",
+        "link": "/record/12/1216"
+      },
+      {
+        "text": "1215",
+        "link": "/record/12/1215"
+      },
+      {
+        "text": "1214",
+        "link": "/record/12/1214"
+      },
+      {
+        "text": "1213",
+        "link": "/record/12/1213"
+      },
+      {
+        "text": "1211",
+        "link": "/record/12/1211"
+      },
+      {
+        "text": "1210",
+        "link": "/record/12/1210"
+      },
+      {
+        "text": "1209",
+        "link": "/record/12/1209"
+      },
+      {
+        "text": "1208",
+        "link": "/record/12/1208"
+      },
+      {
+        "text": "1207",
+        "link": "/record/12/1207"
+      },
+      {
+        "text": "1206",
+        "link": "/record/12/1206"
+      },
+      {
+        "text": "1205",
+        "link": "/record/12/1205-week-summary"
+      },
+      {
+        "text": "1203",
+        "link": "/record/12/1203"
+      },
+      {
+        "text": "1202",
+        "link": "/record/12/1202"
+      },
+      {
+        "text": "1201",
+        "link": "/record/12/1201"
+      }
+    ]
+  },
+  {
+    "text": "11月",
+    "children": [
+      {
+        "text": "1130",
+        "link": "/record/11/1130"
+      },
+      {
+        "text": "1129",
+        "link": "/record/11/1129"
+      },
+      {
+        "text": "1128",
+        "link": "/record/11/1128"
+      },
+      {
+        "text": "1127-周总结",
+        "link": "/record/11/1127-week-summary"
+      },
+      {
+        "text": "1125",
+        "link": "/record/11/1125"
+      },
+      {
+        "text": "1123",
+        "link": "/record/11/1123"
+      },
+      {
+        "text": "1122",
+        "link": "/record/11/1122"
+      },
+      {
+        "text": "1118",
+        "link": "/record/11/1118"
+      },
+      {
+        "text": "1117",
+        "link": "/record/11/1117"
+      },
+      {
+        "text": "1116",
+        "link": "/record/11/1116"
+      },
+      {
+        "text": "1115",
+        "link": "/record/11/1115"
+      },
+      {
+        "text": "1114",
+        "link": "/record/11/1114"
+      },
+      {
+        "text": "1113-周总结",
+        "link": "/record/11/1113-week-summary"
+      },
+      {
+        "text": "1112",
+        "link": "/record/11/1112"
+      },
+      {
+        "text": "1111",
+        "link": "/record/11/1111"
+      },
+      {
+        "text": "1110",
+        "link": "/record/11/1110"
+      },
+      {
+        "text": "1109",
+        "link": "/record/11/1109"
+      },
+      {
+        "text": "1108",
+        "link": "/record/11/1108"
+      },
+      {
+        "text": "1106",
+        "link": "/record/11/1106"
+      },
+      {
+        "text": "1105",
+        "link": "/record/11/1105"
+      },
+      {
+        "text": "1104",
+        "link": "/record/11/1104"
+      },
+      {
+        "text": "1103",
+        "link": "/record/11/1103"
+      },
+      {
+        "text": "1102",
+        "link": "/record/11/1102"
+      },
+      {
+        "text": "1101",
+        "link": "/record/11/1101"
+      }
+    ]
+  },
+  {
+    "text": "10月",
+    "children": [
+      {
+        "text": "1028",
+        "link": "/record/10/1028"
+      },
+      {
+        "text": "1027",
+        "link": "/record/10/1027"
+      },
+      {
+        "text": "1026",
+        "link": "/record/10/1026"
+      },
+      {
+        "text": "1025",
+        "link": "/record/10/1025"
+      },
+      {
+        "text": "1021",
+        "link": "/record/10/1021"
+      },
+      {
+        "text": "1020",
+        "link": "/record/10/1020"
+      },
+      {
+        "text": "1019",
+        "link": "/record/10/1019"
+      },
+      {
+        "text": "1018",
+        "link": "/record/10/1018"
+      },
+      {
+        "text": "1017-周总结",
+        "link": "/record/10/1017-week-summary"
+      },
+      {
+        "text": "1015",
+        "link": "/record/10/1015"
+      },
+      {
+        "text": "1014",
+        "link": "/record/10/1014"
+      },
+      {
+        "text": "1013",
+        "link": "/record/10/1013"
+      },
+      {
+        "text": "1012",
+        "link": "/record/10/1012"
+      },
+      {
+        "text": "1011",
+        "link": "/record/10/1011"
+      },
+      {
+        "text": "1010",
+        "link": "/record/10/1010"
+      },
+      {
+        "text": "1009",
+        "link": "/record/10/1009"
+      },
+      {
+        "text": "1008",
+        "link": "/record/10/1008"
+      }
+    ]
+  },
+  {
+    "text": "9月",
+    "children": [
+      {
+        "text": "0928-周总结",
+        "link": "/record/09/0928-week-summary"
+      },
+      {
+        "text": "0928",
+        "link": "/record/09/0928"
+      },
+      {
+        "text": "0927",
+        "link": "/record/09/0927"
+      },
+      {
+        "text": "0926",
+        "link": "/record/09/0926"
+      },
+      {
+        "text": "0924",
+        "link": "/record/09/0924"
+      },
+      {
+        "text": "0923",
+        "link": "/record/09/0923"
+      },
+      {
+        "text": "0922",
+        "link": "/record/09/0922"
+      },
+      {
+        "text": "0918",
+        "link": "/record/09/0918"
+      },
+      {
+        "text": "0917",
+        "link": "/record/09/0917"
+      },
+      {
+        "text": "0916",
+        "link": "/record/09/0916"
+      },
+      {
+        "text": "0915",
+        "link": "/record/09/0915"
+      },
+      {
+        "text": "0914",
+        "link": "/record/09/0914"
+      },
+      {
+        "text": "0913",
+        "link": "/record/09/0913"
+      },
+      {
+        "text": "0910",
+        "link": "/record/09/0910"
+      },
+      {
+        "text": "0909",
+        "link": "/record/09/0909"
+      },
+      {
+        "text": "0908",
+        "link": "/record/09/0908"
+      },
+      {
+        "text": "0907",
+        "link": "/record/09/0907"
+      },
+      {
+        "text": "0906",
+        "link": "/record/09/0906"
+      },
+      {
+        "text": "0905-周总结",
+        "link": "/record/09/0905-week-summary"
+      },
+      {
+        "text": "0902",
+        "link": "/record/09/0902"
+      },
+      {
+        "text": "0901",
+        "link": "/record/09/0901"
+      }
+    ]
+  },
+  {
+    "text": "8月",
+    "children": [
+      {
+        "text": "0831",
+        "link": "/record/08/0831"
+      },
+      {
+        "text": "0830",
+        "link": "/record/08/0830"
+      },
+      {
+        "text": "0825",
+        "link": "/record/08/0825"
+      },
+      {
+        "text": "0824",
+        "link": "/record/08/0824"
+      },
+      {
+        "text": "0823",
+        "link": "/record/08/0823"
+      },
+      {
+        "text": "0822-周总结",
+        "link": "/record/08/0822-week-summary"
+      },
+      {
+        "text": "0819",
+        "link": "/record/08/0819"
+      },
+      {
+        "text": "0818",
+        "link": "/record/08/0818"
+      },
+      {
+        "text": "0817",
+        "link": "/record/08/0817"
+      },
+      {
+        "text": "0816",
+        "link": "/record/08/0816"
+      },
+      {
+        "text": "0815-周总结",
+        "link": "/record/08/0815-week-summary"
+      },
+      {
+        "text": "0812",
+        "link": "/record/08/0812"
+      },
+      {
+        "text": "0811",
+        "link": "/record/08/0811"
+      },
+      {
+        "text": "0810",
+        "link": "/record/08/0810"
+      },
+      {
+        "text": "0809",
+        "link": "/record/08/0809"
+      },
+      {
+        "text": "0808-周总结",
+        "link": "/record/08/0808-week-summary"
+      },
+      {
+        "text": "0807",
+        "link": "/record/08/0807"
+      },
+      {
+        "text": "0805",
+        "link": "/record/08/0805"
+      },
+      {
+        "text": "0804",
+        "link": "/record/08/0804"
+      },
+      {
+        "text": "0803",
+        "link": "/record/08/0803"
+      },
+      {
+        "text": "0802",
+        "link": "/record/08/0802"
+      },
+      {
+        "text": "0801-周总结",
+        "link": "/record/08/0801-week-summary"
+      }
+    ]
+  },
+  {
+    "text": "7月",
+    "children": [
+      {
+        "text": "7月总结",
+        "link": "/record/07/0799-end"
+      },
+      {
+        "text": "0730",
+        "link": "/record/07/0730"
+      },
+      {
+        "text": "0729",
+        "link": "/record/07/0729"
+      },
+      {
+        "text": "0728",
+        "link": "/record/07/0728"
+      },
+      {
+        "text": "0727",
+        "link": "/record/07/0727"
+      },
+      {
+        "text": "0726",
+        "link": "/record/07/0726"
+      },
+      {
+        "text": "0723",
+        "link": "/record/07/0723"
+      },
+      {
+        "text": "0722",
+        "link": "/record/07/0722"
+      },
+      {
+        "text": "0721",
+        "link": "/record/07/0721"
+      },
+      {
+        "text": "0720",
+        "link": "/record/07/0720"
+      },
+      {
+        "text": "0719",
+        "link": "/record/07/0719"
+      },
+      {
+        "text": "0718-周总结",
+        "link": "/record/07/0718-week-summary"
+      },
+      {
+        "text": "0716",
+        "link": "/record/07/0716"
+      },
+      {
+        "text": "0715",
+        "link": "/record/07/0715"
+      },
+      {
+        "text": "0714",
+        "link": "/record/07/0714"
+      },
+      {
+        "text": "0713",
+        "link": "/record/07/0713"
+      },
+      {
+        "text": "0712",
+        "link": "/record/07/0712"
+      },
+      {
+        "text": "0711-周总结",
+        "link": "/record/07/0711-week-summary"
+      },
+      {
+        "text": "0709",
+        "link": "/record/07/0709"
+      },
+      {
+        "text": "0708",
+        "link": "/record/07/0708"
+      },
+      {
+        "text": "0707",
+        "link": "/record/07/0707"
+      },
+      {
+        "text": "0706",
+        "link": "/record/07/0706"
+      },
+      {
+        "text": "0705",
+        "link": "/record/07/0705"
+      },
+      {
+        "text": "0704-周总结",
+        "link": "/record/07/0704-week-summary"
+      },
+      {
+        "text": "0703",
+        "link": "/record/07/0703"
+      },
+      {
+        "text": "0702",
+        "link": "/record/07/0702"
+      },
+      {
+        "text": "0701",
+        "link": "/record/07/0701"
+      }
+    ]
+  },
+  {
+    "text": "6月",
+    "children": [
+      {
+        "text": "0630",
+        "link": "/record/06/0630"
+      },
+      {
+        "text": "0629",
+        "link": "/record/06/0629"
+      },
+      {
+        "text": "0628",
+        "link": "/record/06/0628"
+      },
+      {
+        "text": "0627-周总结",
+        "link": "/record/06/0627-week-summary"
+      },
+      {
+        "text": "0625",
+        "link": "/record/06/0625"
+      },
+      {
+        "text": "0624",
+        "link": "/record/06/0624"
+      },
+      {
+        "text": "0623",
+        "link": "/record/06/0623"
+      },
+      {
+        "text": "0622",
+        "link": "/record/06/0622"
+      },
+      {
+        "text": "0621",
+        "link": "/record/06/0621"
+      },
+      {
+        "text": "0620-周总结",
+        "link": "/record/06/0620-week-summary"
+      },
+      {
+        "text": "0618",
+        "link": "/record/06/0618"
+      },
+      {
+        "text": "0617",
+        "link": "/record/06/0617"
+      },
+      {
+        "text": "0616",
+        "link": "/record/06/0616"
+      },
+      {
+        "text": "0615",
+        "link": "/record/06/0615"
+      },
+      {
+        "text": "0614-周总结",
+        "link": "/record/06/0614-week-summary"
+      },
+      {
+        "text": "0611",
+        "link": "/record/06/0611"
+      },
+      {
+        "text": "0610",
+        "link": "/record/06/0610"
+      },
+      {
+        "text": "0609",
+        "link": "/record/06/0609"
+      },
+      {
+        "text": "0608",
+        "link": "/record/06/0608"
+      },
+      {
+        "text": "0607",
+        "link": "/record/06/0607"
+      },
+      {
+        "text": "0606-周总结",
+        "link": "/record/06/0606-week-summary"
+      },
+      {
+        "text": "0604",
+        "link": "/record/06/0604"
+      },
+      {
+        "text": "0603",
+        "link": "/record/06/0603"
+      },
+      {
+        "text": "0602",
+        "link": "/record/06/0602"
+      },
+      {
+        "text": "0601",
+        "link": "/record/06/0601"
+      }
+    ]
+  },
+  {
+    "text": "5月",
+    "children": [
+      {
+        "text": "0531",
+        "link": "/record/05/0531"
+      },
+      {
+        "text": "0530-周总结",
+        "link": "/record/05/0530-week-summary"
+      },
+      {
+        "text": "0528",
+        "link": "/record/05/0528"
+      },
+      {
+        "text": "0527",
+        "link": "/record/05/0527"
+      },
+      {
+        "text": "0526",
+        "link": "/record/05/0526"
+      },
+      {
+        "text": "0525",
+        "link": "/record/05/0525"
+      },
+      {
+        "text": "0524",
+        "link": "/record/05/0524"
+      },
+      {
+        "text": "0523-周总结",
+        "link": "/record/05/0523-week-summary"
+      },
+      {
+        "text": "0521",
+        "link": "/record/05/0521"
+      },
+      {
+        "text": "0520",
+        "link": "/record/05/0520"
+      },
+      {
+        "text": "0519",
+        "link": "/record/05/0519"
+      },
+      {
+        "text": "0518",
+        "link": "/record/05/0518"
+      },
+      {
+        "text": "0517",
+        "link": "/record/05/0517"
+      },
+      {
+        "text": "0516-周总结",
+        "link": "/record/05/0516-week-summary"
+      },
+      {
+        "text": "0514",
+        "link": "/record/05/0514"
+      },
+      {
+        "text": "0513",
+        "link": "/record/05/0513"
+      },
+      {
+        "text": "0512",
+        "link": "/record/05/0512"
+      },
+      {
+        "text": "0511",
+        "link": "/record/05/0511"
+      },
+      {
+        "text": "0510",
+        "link": "/record/05/0510"
+      },
+      {
+        "text": "0509",
+        "link": "/record/05/0509"
+      },
+      {
+        "text": "0508",
+        "link": "/record/05/0508"
+      },
+      {
+        "text": "0507",
+        "link": "/record/05/0507"
+      },
+      {
+        "text": "0506",
+        "link": "/record/05/0506"
+      }
+    ]
+  }
+]
