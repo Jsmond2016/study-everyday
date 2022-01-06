84b1ebfcdb1b3b484a981652ec409e34a0c09a90
Jsmond2016 <Jsmond2016@gmail.com>
2022-01-06 23:53:12 +0800

    add 0106

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,6 +3,10 @@ module.exports = [
     "text": "1月",
     "children": [
       {
+        "text": "0106",
+        "link": "/record-2022/01/0106"
+      },
+      {
         "text": "0105",
         "link": "/record-2022/01/0105"
       },
