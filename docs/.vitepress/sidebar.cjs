aea64fcc1038bbbaf8938404322da66b36db7aab
Jsmond2016 <Jsmond2016@gmail.com>
2023-08-28 23:14:47 +0800

    docs: 新增 8-28 笔记

--- docs/.vitepress/sidebar.cjs
+++ docs/.vitepress/sidebar.cjs
@@ -3,9 +3,14 @@ module.exports = [
     "text": "8月",
     "items": [
       {
+        "text": "0828",
+        "link": "/record-2023/08/0828"
+      },
+      {
         "text": "0827",
         "link": "/record-2023/08/0827"
-      }
+      },
+      
     ]
   }
 ]
