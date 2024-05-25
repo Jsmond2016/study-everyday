010fdab4c6a8462517853186e0967e561a3c4ba0
Jsmond2016 <jsmond2016@gmail.com>
2024-05-25 11:19:29 +0800

    feat: add 0525

--- docs/.vitepress/sidebar.cjs
+++ docs/.vitepress/sidebar.cjs
@@ -1,79 +1,83 @@
 module.exports = [
   {
-    text: "5月",
-    items: [
+    "text": "5月",
+    "items": [
       {
-        text: "0510",
-        link: "/record-2024/05/0510",
+        "text": "0525",
+        "link": "/record-2024/05/0525"
       },
-    ],
+      {
+        "text": "0510",
+        "link": "/record-2024/05/0510"
+      }
+    ]
   },
   {
-    text: "4月",
-    items: [
+    "text": "4月",
+    "items": [
       {
-        text: "0422",
-        link: "/record-2024/04/0422",
+        "text": "0422",
+        "link": "/record-2024/04/0422"
       },
       {
-        text: "0428",
-        link: "/record-2024/04/0428",
+        "text": "0428",
+        "link": "/record-2024/04/0428"
       },
       {
-        text: "0416",
-        link: "/record-2024/04/0416",
+        "text": "0416",
+        "link": "/record-2024/04/0416"
       },
       {
-        text: "0410",
-        link: "/record-2024/04/0410",
-      },
-    ],
+        "text": "0410",
+        "link": "/record-2024/04/0410"
+      }
+    ]
   },
   {
-    text: "3月",
-    items: [
+    "text": "3月",
+    "items": [
       {
-        text: "0330",
-        link: "/record-2024/03/0330",
+        "text": "0330",
+        "link": "/record-2024/03/0330"
       },
       {
-        text: "0326",
-        link: "/record-2024/03/0326",
+        "text": "0326",
+        "link": "/record-2024/03/0326"
       },
       {
-        text: "0315",
-        link: "/record-2024/03/0315",
-      },
-    ],
+        "text": "0315",
+        "link": "/record-2024/03/0315"
+      }
+    ]
   },
   {
-    text: "2月",
-    items: [
+    "text": "2月",
+    "items": [
       {
-        text: "0221",
-        link: "/record-2024/02/0221",
+        "text": "0221",
+        "link": "/record-2024/02/0221"
       },
       {
-        text: "0220",
-        link: "/record-2024/02/0220",
+        "text": "0220",
+        "link": "/record-2024/02/0220"
       },
       {
-        text: "0219",
-        link: "/record-2024/02/0219",
+        "text": "0219",
+        "link": "/record-2024/02/0219"
       },
       {
-        text: "0209",
-        link: "/record-2024/02/0209",
-      },
-    ],
+        "text": "0209",
+        "link": "/record-2024/02/0209"
+      }
+    ]
   },
   {
-    text: "1月",
-    items: [
+    "text": "1月",
+    "items": [
       {
-        text: "0107",
-        link: "/record-2024/01/0107",
-      },
-    ],
-  },
-];
+        "text": "0107",
+        "link": "/record-2024/01/0107"
+      }
+    ]
+  }
+]
