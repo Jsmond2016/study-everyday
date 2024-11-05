d6feeec15250256882504c441d08220ea3ce8ab4
Jsmond2016 <jsmond2016@gmail.com>
2024-11-05 23:18:37 +0800

    feat: 增加笔记

--- docs/.vitepress/sidebar.cjs
+++ docs/.vitepress/sidebar.cjs
@@ -1,182 +1,201 @@
 module.exports = [
   {
-    text: "10月",
-    items: [
+    "text": "11月",
+    "items": [
       {
-        text: "1011-todo",
-        link: "/record-2024/10/1011",
-      },
-    ],
+        "text": "1105-todo",
+        "link": "/record-2024/11/1105"
+      }
+    ]
   },
   {
-    text: "9月",
-    items: [
+    "text": "10月",
+    "items": [
       {
-        text: "0922",
-        link: "/record-2024/09/0922",
+        "text": "1026-todo",
+        "link": "/record-2024/10/1026"
       },
       {
-        text: "0915",
-        link: "/record-2024/09/0915",
+        "text": "1016",
+        "link": "/record-2024/10/1016"
       },
       {
-        text: "0910",
-        link: "/record-2024/09/0910",
+        "text": "1011",
+        "link": "/record-2024/10/1011"
       },
       {
-        text: "0908",
-        link: "/record-2024/09/0908",
-      },
-    ],
+        "text": "1005",
+        "link": "/record-2024/10/1005"
+      }
+    ]
   },
   {
-    text: "8月",
-    items: [
+    "text": "9月",
+    "items": [
       {
-        text: "0824-todo",
-        link: "/record-2024/08/0824",
+        "text": "0922",
+        "link": "/record-2024/09/0922"
       },
       {
-        text: "0817-todo",
-        link: "/record-2024/08/0817",
+        "text": "0915",
+        "link": "/record-2024/09/0915"
       },
       {
-        text: "0810-todo",
-        link: "/record-2024/08/0810",
+        "text": "0910",
+        "link": "/record-2024/09/0910"
       },
       {
-        text: "0803",
-        link: "/record-2024/08/0803",
-      },
-    ],
-  },
-  
- 
-  {
-    text: "7月",
-    items: [
-      {
-        text: "0723",
-        link: "/record-2024/07/0723",
-      },
-      {
-        text: "0720",
-        link: "/record-2024/07/0720",
-      },
-      {
-        text: "0715",
-        link: "/record-2024/07/0715",
-      },
-      {
-        text: "0701",
-        link: "/record-2024/07/0701",
-      },
-    ],
+        "text": "0908",
+        "link": "/record-2024/09/0908"
+      }
+    ]
   },
   {
-    text: "6月",
-    items: [
+    "text": "8月",
+    "items": [
       {
-        text: "0628",
-        link: "/record-2024/06/06028",
+        "text": "0824-todo",
+        "link": "/record-2024/08/0824"
       },
       {
-        text: "0622",
-        link: "/record-2024/06/06022",
+        "text": "0817-todo",
+        "link": "/record-2024/08/0817"
       },
       {
-        text: "0612",
-        link: "/record-2024/06/06012",
+        "text": "0810-todo",
+        "link": "/record-2024/08/0810"
       },
       {
-        text: "0601",
-        link: "/record-2024/06/0601",
-      },
-    ],
+        "text": "0803",
+        "link": "/record-2024/08/0803"
+      }
+    ]
   },
   {
-    text: "5月",
-    items: [
+    "text": "7月",
+    "items": [
       {
-        text: "0525",
-        link: "/record-2024/05/0525",
+        "text": "0723",
+        "link": "/record-2024/07/0723"
       },
       {
-        text: "0518",
-        link: "/record-2024/05/0518",
+        "text": "0720",
+        "link": "/record-2024/07/0720"
       },
       {
-        text: "0510",
-        link: "/record-2024/05/0510",
+        "text": "0715",
+        "link": "/record-2024/07/0715"
       },
-    ],
+      {
+        "text": "0701",
+        "link": "/record-2024/07/0701"
+      }
+    ]
   },
   {
-    text: "4月",
-    items: [
+    "text": "6月",
+    "items": [
       {
-        text: "0422",
-        link: "/record-2024/04/0422",
+        "text": "0628",
+        "link": "/record-2024/06/06028"
       },
       {
-        text: "0428",
-        link: "/record-2024/04/0428",
+        "text": "0622",
+        "link": "/record-2024/06/06022"
       },
       {
-        text: "0416",
-        link: "/record-2024/04/0416",
+        "text": "0612",
+        "link": "/record-2024/06/06012"
       },
       {
-        text: "0410",
-        link: "/record-2024/04/0410",
-      },
-    ],
+        "text": "0601",
+        "link": "/record-2024/06/0601"
+      }
+    ]
   },
   {
-    text: "3月",
-    items: [
+    "text": "5月",
+    "items": [
       {
-        text: "0330",
-        link: "/record-2024/03/0330",
+        "text": "0525",
+        "link": "/record-2024/05/0525"
       },
       {
-        text: "0326",
-        link: "/record-2024/03/0326",
+        "text": "0518",
+        "link": "/record-2024/05/0518"
       },
       {
-        text: "0315",
-        link: "/record-2024/03/0315",
-      },
-    ],
+        "text": "0510",
+        "link": "/record-2024/05/0510"
+      }
+    ]
   },
   {
-    text: "2月",
-    items: [
+    "text": "4月",
+    "items": [
       {
-        text: "0221",
-        link: "/record-2024/02/0221",
+        "text": "0422",
+        "link": "/record-2024/04/0422"
       },
       {
-        text: "0220",
-        link: "/record-2024/02/0220",
+        "text": "0428",
+        "link": "/record-2024/04/0428"
       },
       {
-        text: "0219",
-        link: "/record-2024/02/0219",
+        "text": "0416",
+        "link": "/record-2024/04/0416"
       },
       {
-        text: "0209",
-        link: "/record-2024/02/0209",
-      },
-    ],
+        "text": "0410",
+        "link": "/record-2024/04/0410"
+      }
+    ]
   },
   {
-    text: "1月",
-    items: [
+    "text": "3月",
+    "items": [
       {
-        text: "0107",
-        link: "/record-2024/01/0107",
+        "text": "0330",
+        "link": "/record-2024/03/0330"
       },
-    ],
+      {
+        "text": "0326",
+        "link": "/record-2024/03/0326"
+      },
+      {
+        "text": "0315",
+        "link": "/record-2024/03/0315"
+      }
+    ]
   },
-];
+  {
+    "text": "2月",
+    "items": [
+      {
+        "text": "0221",
+        "link": "/record-2024/02/0221"
+      },
+      {
+        "text": "0220",
+        "link": "/record-2024/02/0220"
+      },
+      {
+        "text": "0219",
+        "link": "/record-2024/02/0219"
+      },
+      {
+        "text": "0209",
+        "link": "/record-2024/02/0209"
+      }
+    ]
+  },
+  {
+    "text": "1月",
+    "items": [
+      {
+        "text": "0107",
+        "link": "/record-2024/01/0107"
+      }
+    ]
+  }
+]
