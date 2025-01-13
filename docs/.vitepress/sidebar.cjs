9f07f8479a45052eab5e8520db0dd6b370f34e1a
Jsmond2016 <jsmond2016@gmail.com>
2025-01-13 22:50:13 +0800

    feat(note): add 0111

--- docs/.vitepress/sidebar.cjs
+++ docs/.vitepress/sidebar.cjs
@@ -3,6 +3,10 @@ module.exports = [
     "text": "1月",
     "items": [
       {
+        "text": "0111",
+        "link": "/record-2025/01/0111"
+      },
+      {
         "text": "0108",
         "link": "/record-2025/01/0108"
       }
