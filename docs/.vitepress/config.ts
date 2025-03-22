3781fa0028ecdc5c80f6c9b3765e28e6f6bd49e3
jsmond2016 <jsmond2016@gmail.com>
2025-03-22 22:14:33 +0800

    feat: 增加 logo 图标

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   base: "/study-everyday/",
   description: "My Study Notes for everyday",
   themeConfig: {
+    logo: "/logo.png",
     // https://vitepress.dev/reference/default-theme-config
     nav: nav(),
     sidebar: {
